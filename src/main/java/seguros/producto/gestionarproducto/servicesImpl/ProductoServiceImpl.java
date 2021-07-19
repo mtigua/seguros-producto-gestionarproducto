b75dfc0fb6a970512ff2b94f8369fb860552b8bd
ariel.brito <ariel.britomedina@cencosudscotiabank.cl>
2021-07-19 07:27:29 -0400

    Ajuste en registro de paso inicial

--- src/main/java/seguros/producto/gestionarproducto/servicesImpl/ProductoServiceImpl.java
+++ src/main/java/seguros/producto/gestionarproducto/servicesImpl/ProductoServiceImpl.java
@@ -2490,6 +2490,14 @@ public class ProductoServiceImpl implements ProductoService {
 							}
 						});
 					}
+					
+					Long idTipoSeguro= producto.getTipoSeguro();
+					if(idTipoSeguro!=null && !VALUE_UNDEFINED.equals(idTipoSeguro)) {
+						TipoSeguro tipoSeguro= tipoSeguroRepository.getOne(idTipoSeguro);
+						if(tipoSeguro.getId()!=null) {
+							productoEntity.setTipoSeguro(tipoSeguro);
+						}
+					}
 				
 					productoEntity.setIdCiaNegocioRamo(idCiaNegocioRamo);
 					productoEntity.setNemot(requestNemotecnico);
@@ -2557,6 +2565,14 @@ public class ProductoServiceImpl implements ProductoService {
 									productoEntity.addCanal(canalEntity);
 								}
 							});
+						}
+						
+						Long idTipoSeguro= producto.getTipoSeguro();
+						if(idTipoSeguro!=null) {
+							TipoSeguro tipoSeguro= tipoSeguroRepository.getOne(idTipoSeguro);
+							if(tipoSeguro.getId()!=null) {
+								productoEntity.setTipoSeguro(tipoSeguro);
+							}
 						}
 					
 						productoEntity.setId(id);	
@@ -2872,6 +2888,10 @@ public class ProductoServiceImpl implements ProductoService {
     			  
     		   }
     		   
+    		   TipoSeguro tipoSeguro =    productoEntity.getTipoSeguro();
+    		   if(tipoSeguro!=null) {
+    			   form.setTipoSeguro(tipoSeguro.getId());
+    		   }
     		  // InfoProductoDto info= productoRepository.getInfoProducto(id);
     	   }
     	   else {
